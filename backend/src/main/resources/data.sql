30c068c60302e205ee71825a8b2688a184112133
chickencoc <solutw@gmail.com>
2023-09-30 13:55:08 +0900

    chore : application.yml jpa 및 sql 설정, data.sql 추가

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Company Sample
+INSERT INTO company (`name`, `country`, `city`) VALUES ('원티드랩', '한국', '서울');
+INSERT INTO company (`name`, `country`, `city`) VALUES ('원티드코리아', '한국', '부산');
+INSERT INTO company (`name`, `country`, `city`) VALUES ('네이버', '한국', '성남');
+INSERT INTO company (`name`, `country`, `city`) VALUES ('카카오', '한국', '제주');
+
+-- Member Sample
+INSERT INTO member (`email`) VALUES ('user001@gmail.com');
+INSERT INTO member (`email`) VALUES ('user003@gmail.com');
+INSERT INTO member (`email`) VALUES ('user005@gmail.com');
